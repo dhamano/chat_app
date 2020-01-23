1e667d986516030d44b533c473ad7aca437a122d
Dustin <clerin+gitkraken@gmail.com>
2020-01-23 15:32:58 -0800

    WIP authentication for login and reg

--- node_backend/api/routes/auth.js
+++ node_backend/api/routes/auth.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const bcrypt = require*('bcrypt');
+const mw = require('../middleware');
+
+router.post('/register', mw.checkUserInfo, async (req, res) => {
+    try {
+        req.user.password = getHash(req.user.password);
+
+        const user = await Users.add(req.user);
+        res.status(201).json({ 'message': 'User created', user });
+    }
+    catch(err) {
+        res.status(500).json({ error: 'There was a problem adding the user to the server.' });
+    }
+})
