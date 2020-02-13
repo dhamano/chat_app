986c8eb57357c0eefc57c4db9128f69b73fabee1
Dustin <clerin+gitkraken@gmail.com>
2020-02-12 17:52:38 -0800

    adding changes. need to change nodejs code to work with both REST and WebSocketServer. need to change the way the frontend connects to server

--- node_backend/index.js
+++ node_backend/index.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-const server = require('./api/server');
-
 const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`\n*** API running on port ${port} ***\n`));
 
